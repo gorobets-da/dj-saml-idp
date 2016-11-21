654f38dbe10b711761e15e2b083dd282234fa687
Danielle Madeley <danielle@madeley.id.au>
2016-11-21 13:58:08 +1100

    Remove use of django.conf.urls.patterns()
    Removed from Django 1.10.

--- saml2idp/urls.py
+++ saml2idp/urls.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .views import descriptor, login_begin, login_init, login_process, logout
 from .metadata import get_deeplink_resources
 
 def deeplink_url_patterns(
-    prefix='',
     url_base_pattern=r'^init/%s/$',
     login_init_func=login_init,
     ):
@@ -18,17 +17,17 @@ def deeplink_url_patterns(
     resources = get_deeplink_resources()
     new_patterns = []
     for resource in resources:
-        new_patterns += patterns(prefix,
+        new_patterns += [
             url( url_base_pattern % resource,
                  login_init_func,
                  {
                     'resource': resource,
                  },
             )
-        )
+        ]
     return new_patterns
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^login/$', login_begin, name="saml_login_begin"),
     url(r'^login/process/$', login_process, name='saml_login_process'),
     url(r'^logout/$', logout, name="saml_logout"),
@@ -37,6 +36,6 @@ urlpatterns = patterns('',
     url(r'^init/(?P<resource>\w+)/(?P<target>\w+)/$',
         login_init,
         name="login_init"),
-)
+]
 # Issue 13 - Add new automagically-created URLs for deeplinks:
 urlpatterns += deeplink_url_patterns()
